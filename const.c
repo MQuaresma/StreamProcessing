ba436eb4465dc3822e11d8bb9a4265bcbd307769
José Martins <jcm300@live.com.pt>
2017-05-12 12:00:41 +0100

    const and spawn funcs writing to stdout in lines and reading from stdin

--- const.c
+++ const.c
@@ -1,28 +1,24 @@
-#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <fcntl.h>
 
 int main (int argc, char *argv[]){
-	int ret=1;
+	int eV=1;
 
 	if(argc==2){
-            char c;
-            int fd1,fd2;
+    	char c;
+        int i=0, w;
+		char ret[PIPE_BUF];
    
-            fd1 = open("file",O_RDONLY);
-            if(fd1>0){
-                    while(read(fd1,&c,1)>0){
-                            if(c=='\n'){
-                                    write(1,":",1);
-                                    write(1,argv[1],strlen(argv[1]));
-                                    write(1,"\n",1);
-                            }else write(1,&c,1);
-                    }
-                    close(fd1);
-                    close(fd2);
-                    ret=0;
-            }
-	}
-	return ret;	
+        while(read(0,&c,1)>0){
+			if(c=='\n'){
+				ret[i++]=':';
+				for(w=0;w<strlen(argv[1]);w++) ret[i++]=argv[1][w];
+				ret[i++]='\n';
+				write(1,ret,i);
+				i=0;
+			}else ret[i++]=c;
+		}
+		eV=0;
+    }
+	return eV;
 }
--- spawn.c
+++ spawn.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include <fcntl.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 
@@ -26,10 +25,11 @@ int elem (int *arr, int n, int x){
 }
 
 int main (int argc, char *argv[]){
-	int ret=1;
+	int eV=1;
 
 	if(argc>1){
 		int i, v;
+
 		char **args = (char**)calloc(argc, sizeof(char));
 		for(i=1, v=0; i<argc; i++){
 			if((*argv[i])=='$') v++;
@@ -41,52 +41,50 @@ int main (int argc, char *argv[]){
 				for(i=1, v=0; i<argc; i++){
 						if(*argv[i]=='$') {
 								col[v]=atoi(argv[i]+1);
-								pos[v]=i-1;
-								v++;
+								pos[v++]=i-1;
 						}
 				}
 		}
 
 		char c;
-		char n[2];
 		char aux[10]; 
-		int fd1, fd2, y=0, w=0, exitValue;
-	
-		fd1 = open("file",O_RDONLY);
-		if(fd1>0){
-			i=1;
-			while(read(fd1,&c,1)>0){
-				if(c=='\n'){
-					exitValue=mySystem(args);
-					sprintf(n,"%d",exitValue);
-					write(1,":",1);
-					write(1,n,strlen(n));
-					write(1,"\n",1);
-				}else if(c==':') {
-					if(v && y!=0){
-						aux[y]='\0';
-						args[pos[w]]=strdup(aux);
-						w++;
-						y=0;
-					}
-					write(1,&c,1);
-					i++;
-				}else{
-					if(v && elem(col,v,i)){
-						aux[y]=c;
-						y++;           
-					}
-					write(1,&c,1);
+		int y=0, w=0, nCol, exitValue;
+		char ret[PIPE_BUF];
+
+		i=0;
+		nCol=1;
+		while(read(0,&c,1)>0){
+			if(c=='\n'){
+				if(v && y!=0){
+					aux[y]='\0';
+                   	args[pos[w++]]=strdup(aux);
+                   	y=0;
 				}
+				exitValue=mySystem(args);
+				sprintf(aux,"%d",exitValue);
+				ret[i++]=':';
+				for(w=0;w<strlen(aux); w++) ret[i++]=aux[w];
+				ret[i++]='\n';
+				write(1,ret,i);
+				i=w=0;
+				nCol=1;
+			}else if(c==':') {
+				if(v && y!=0){
+					aux[y]='\0';
+					args[pos[w++]]=strdup(aux);
+					y=0;
+				}
+				ret[i++]=c;
+				nCol++;
+			}else{
+				if(v && elem(col,v,nCol)) aux[y++]=c;
+				ret[i++]=c;
 			}
-			close(fd1);
-			close(fd2);
-			ret=0;
 		}
-		for(i=0;i<argc-1; i++){
-			free(args[i]);
-		}
+		eV=0;
+
+		for(i=0;i<argc-1; i++) free(args[i]);
 		free(args);
 	}
-	return ret;
+	return eV;
 }
